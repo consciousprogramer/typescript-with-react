8008dda3a29452da88e86c06b002c0b8f1adbf6a
ravi shankar <shankarji1729@gmail.com>
2021-05-28 11:22:14 +0530

    added a new feature

---
+++ src/components/Newfeature.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewFeature = () => {
+    return (
+        <div className="">
+            <h1 className="">This is a new feature!</h1>
+        </div>
+    )
+}
+
+export default NewFeature;
