a555fb86b4d11156185bdceef3e8b93baac3d1e6
ravi shankar <shankarji1729@gmail.com>
2021-05-28 10:59:16 +0530

    modified form.tsx and added comment in main.tsx context

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,14 +1,16 @@
 import React, { useRef } from 'react';
+import classes from "./form.module.css"
 
 const Form: React.FC<{ addTodo: (text: string) => void }> = (props) => {
     const inputRef = useRef<HTMLInputElement>(null)
 
     const submitHandler = (event: React.FormEvent) => {
         event.preventDefault()
+        console.log(inputRef.current!.value)
         props.addTodo(inputRef.current!.value)
     }
     return (
-        <div className="">
+        <div className={classes.form}>
             <form action="" onSubmit={submitHandler}>
                 <label htmlFor="task">Enter New Task</label>
                 <input type="text" name="task" id="task" ref={inputRef} />
